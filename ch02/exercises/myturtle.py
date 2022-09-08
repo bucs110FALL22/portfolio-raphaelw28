73090d930427e3ee9f7203b31dfece9fdf3930ac
Raphael Wong <112580938+raphaelw28@users.noreply.github.com>
2022-09-08 00:04:21 +0000

    myturtle

---
+++ ch02/exercises/myturtle.py
@@ -0,0 +1,23 @@
+import turtle
+
+myturtle = turtle.Screen()
+turtle.Screen() = window
+window.exitonlick()
+
+my_turtle.shape("turtle")
+my_turtle.color("purple")
+
+square_length = 50
+square_sides = 4
+angle = 90 / side_squares
+for i in [angle]*square_sides
+  my_turtle.forward(length)
+  my_turtle.left(i)
+
+my_turtle.color("red")
+turtle.penup()
+my_turtle.forward(100)
+turtle.pendown()
+for i in [angle]*square_sides
+  my_turtle.forward(length)
+  my_turtle.left(i)
