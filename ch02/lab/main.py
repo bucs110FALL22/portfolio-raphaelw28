8ce0951461ebb52de3aeaff1ba9bbe4b39c8e484
Raphael Wong <112580938+raphaelw28@users.noreply.github.com>
2022-09-12 19:24:22 +0000

    lab 2

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -10,7 +10,7 @@ classes_per_week = 5
 cost_per_class = float(cost_per_week / classes_per_week)
 cost_per_class = str(cost_per_class)
 print(cost_per_class + " is very expensive!")
-print(weeks, classes, tuition, cost_per_week, classes_per_week, cost_per_class, type(cost_per_class))
+print(weeks, type(weeks), classes, type(classes), tuition, type(tuition), cost_per_week, type(cost_per_week), classes_per_week, type(classes_per_week), cost_per_class, type(cost_per_class))
 
 
 #Part B
