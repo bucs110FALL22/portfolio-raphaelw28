1e93bc7c4aa55abf2706552efa8b90f95505855f
Raphael Wong <112580938+raphaelw28@users.noreply.github.com>
2022-10-11 21:02:33 +0000

    s

---
+++ ch05/exercises/patterns.py
@@ -0,0 +1,21 @@
+#def star_pyramid():
+#  rows = (input("How many rows of stars?"))
+#  rows = int(rows)
+#  for i in range(rows):
+#    rows = ((rows - rows+1)+i)
+#  for i in range(rows):
+#    print(("*")*(i+1))
+
+#star_pyramid()
+
+
+rows = (input("How many rows of stars?"))
+rows = int(rows)
+for i in range(rows, 0, -1):
+  for i in range(0, i):
+    print(("*")*i)
+
+
+
+    
+  
