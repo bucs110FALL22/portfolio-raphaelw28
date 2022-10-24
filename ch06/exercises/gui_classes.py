f0611cc47f985095f6fc2ce869ec66e2052d9922
Raphael Wong <112580938+raphaelw28@users.noreply.github.com>
2022-10-24 21:55:21 +0000

    6

---
+++ ch06/exercises/gui_classes.py
@@ -0,0 +1,10 @@
+class World:
+	def __init__(Stage):
+		“””
+			Initialize the stage object
+               
+               args: pnum [int] the world values  
+		“””
+          World.level = (1, 1) # world and stage
+          World.blocks = 30 # amount of collidable blocks
+          World.mobs = 2 #number of monsters spawned
