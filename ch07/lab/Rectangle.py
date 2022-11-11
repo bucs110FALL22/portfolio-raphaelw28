0f3cf4bf52284be1205ccacacc2c0f1b1c408253
Raphael Wong <112580938+raphaelw28@users.noreply.github.com>
2022-11-11 08:59:47 +0000

    lab7

---
+++ ch07/lab/Rectangle.py
@@ -0,0 +1,18 @@
+class Rectangle:
+  def __init__(self, x, y, height, width):
+    self.x = x
+    self.y = y
+    self.height = height
+    self.width = width
+    if x < 0:
+      self.x = 0
+    if y < 0:
+      self.y = 0
+    if width < 0:
+      self.width = 0
+    if height < 0:
+      self.height = 0
+      
+def __str__(self):
+    return 'x = {}, y = {}, height = {}, width = {}'.format(self.x, self.y, self.height, self.width)
+  
