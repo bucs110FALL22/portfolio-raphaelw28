5f713ba1f0f5f3b0482d04aa7a1a9706c2c4acd3
Raphael Wong <112580938+raphaelw28@users.noreply.github.com>
2022-12-01 05:14:18 +0000

    chapter 7 lab

--- ch07/lab/Rectangle.py
+++ ch07/lab/Rectangle.py
@@ -1,18 +1,16 @@
 class Rectangle:
-  def __init__(self, x, y, height, width):
-    self.x = x
-    self.y = y
-    self.height = height
+  def __init__(self, xcoord, ycoord, height, width):
+    self.x = xcoord
+    self.y = ycoord
     self.width = width
-    if x < 0:
+    self.height = height
+    if xcoord < 0:
       self.x = 0
-    if y < 0:
+    if ycoord < 0:
       self.y = 0
     if width < 0:
       self.width = 0
     if height < 0:
       self.height = 0
-      
-def __str__(self):
-    return 'x = {}, y = {}, height = {}, width = {}'.format(self.x, self.y, self.height, self.width)
-  
+  def __str__(self):
+    return 'xcoord = {}, ycoord = {}, height = {}, width = {}'.format(self.x, self.y, self.height, self.width)
